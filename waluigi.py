76a1aeef5b74192b00cb67c024a9b183ac149c38
Dan Frank <danielhfrank@gmail.com>
2013-02-09 11:41:52 -0500

    fix things up to work with subclasses, add special input and output params

--- waluigi.py
+++ waluigi.py
@@ -1,4 +1,5 @@
 import itertools
+from operator import attrgetter
 
 from mrjob.job import MRJob
 from luigi import Task, Parameter
@@ -19,12 +20,7 @@ class WaluigiTask(Task):
     '''
 
     job_cls = Parameter()
-    runner = WaluigiMRJobParameter('-r', default='local')
-
-    def __init__(self, *args, **kwargs):
-        print self.__dict__
-        super(WaluigiTask, self).__init__(*args, **kwargs)
-        
+    runner = WaluigiMRJobParameter('-r', default='local') 
 
     def run(self):
         opts = self.make_mrjob_opts()
@@ -36,13 +32,26 @@ class WaluigiTask(Task):
 
     @classmethod
     def get_mrjob_params_and_flags(cls):
-        return [(param_name, param.flag) for param_name, param in cls.__dict__.items() 
-            if isinstance(param, WaluigiMRJobParameter)]
+        '''
+        Gets the WaluigiJobParameters from this and all parent classes
+        '''
+        def get_from(clazz):
+            return [(param_name, param.flag) for param_name, param in clazz.__dict__.items() 
+                if isinstance(param, WaluigiMRJobParameter)]
+        return list(itertools.chain(*(get_from(cl) for cl in cls.__mro__)))
 
     def make_mrjob_opts(self):
         prms_and_flags = self.get_mrjob_params_and_flags()
-        return list(itertools.chain(*((flag, getattr(self, param_name)) for param_name, flag
+        base_params = list(itertools.chain(*((flag, getattr(self, param_name)) for param_name, flag
             in prms_and_flags)))
+        # now add special params: input and ouput
+        # yikes, existence of path field not really enforced
+        input_files = map(attrgetter('path'), self.input())
+        output_dir = self.output().path 
+        assert input_files and output_dir, "Must define output and dependencies or direct input"
+        return base_params + ['-o', output_dir] + input_files
+
+
         
 if __name__ == '__main__':
     t = WaluigiTask(runner='df', job_cls=MRWordFreqCount)
