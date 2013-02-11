70d918d24ee526d24ef050aebe14aff056e9c353
Dan Frank <danielhfrank@gmail.com>
2013-02-10 20:27:43 -0500

    cleanup, add support for jobconf vars

--- waluigi.py
+++ waluigi.py
@@ -1,7 +1,6 @@
-import itertools
+from itertools import chain
 from operator import attrgetter
 
-from mrjob.job import MRJob
 from luigi import Task, Parameter
 
 
@@ -19,13 +18,19 @@ class WaluigiTask(Task):
     '''
 
     job_cls = Parameter()
-    runner = WaluigiMRJobParameter('-r', default='') 
+    runner = WaluigiMRJobParameter('-r', default='')
+    conf_path = WaluigiMRJobParameter('-c', default='')
+
+    def __init__(self, *args, **kwargs):
+        super(WaluigiTask, self).__init__(*args, **kwargs)
+        jobconf = (kwargs.get('jobconf', {}))
+        self.opts = self.make_mrjob_opts() + \
+            list(chain(*[('--jobconf', '%s=%s' % (k,v)) for k,v in jobconf.items()]))
+        # print self.opts
+
 
     def run(self):
-        opts = self.make_mrjob_opts()
-        print opts
-        job = self.job_cls(args=opts)
-        assert isinstance(job, MRJob)
+        job = self.job_cls(args=self.opts)
         jobrunner = job.make_runner()
         jobrunner.run()
 
@@ -37,11 +42,11 @@ class WaluigiTask(Task):
         def get_from(clazz):
             return [(param_name, param.flag) for param_name, param in clazz.__dict__.items() 
                 if isinstance(param, WaluigiMRJobParameter)]
-        return list(itertools.chain(*(get_from(cl) for cl in cls.__mro__)))
+        return list(chain(*(get_from(cl) for cl in cls.__mro__)))
 
     def make_mrjob_opts(self):
         prms_and_flags = self.get_mrjob_params_and_flags()
-        base_params = list(itertools.chain(*((flag, getattr(self, param_name)) for param_name, flag
+        base_params = list(chain(*((flag, getattr(self, param_name)) for param_name, flag
             in prms_and_flags if getattr(self, param_name))))
         # now add special params: input and ouput
         # yikes, existence of path field not really enforced
